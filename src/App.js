22272f0fa2dfc1b5399f3b86b3844906ecc89d10
Robson Candido dos Santos Alves <robsondesenvolvimento@gmail.com>
2021-06-10 21:03:55 -0300

    Add form markup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <div>
+          <label htmlFor="number1">Número 1</label>
+          <input type="number" name="number1" placeholder="Primeiro número" required />
+        </div>
+        <div>
+        <label htmlFor="number2">Número 2</label>
+          <input type="number" name="number2" placeholder="Segundo número" required />
+        </div>
+        <div>
+          <button>Somar</button>
+        </div>
+
+      </form>
     </div>
   );
 }
