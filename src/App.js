4de463498c4c7f14b882bf5f4b9457eb54bf37b3
Robson Candido dos Santos Alves <robsondesenvolvimento@gmail.com>
2021-06-11 01:17:36 -0300

    Formater prettier

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
-import { useRef, useState } from 'react';
-import './App.css';
-import { sum } from './helpers/calculator'
+import { useRef, useState } from "react";
+import "./App.css";
+import { sum } from "./helpers/calculator";
 
 function App() {
   const number1 = useRef(null);
   const number2 = useRef(null);
-  const [result,setResult] = useState(null);
+  const [result, setResult] = useState(null);
 
-  const submitHandler = (event) => {
+  const submitHandler = event => {
     event.preventDefault();
     const firstValue = number1.current.value;
     const secondValue = number2.current.value;
 
     setResult(sum(firstValue, secondValue));
-  }
+  };
 
   return (
     <div className="App">
-      <form onSubmit={(event) => submitHandler(event)}>
+      <form onSubmit={event => submitHandler(event)}>
         <div>
           <label htmlFor="number1">Número 1</label>
-          <input type="number" name="number1" placeholder="Primeiro número" required ref={number1}/>
+          <input
+            type="number"
+            name="number1"
+            placeholder="Primeiro número"
+            required
+            ref={number1}
+          />
         </div>
         <div>
-        <label htmlFor="number2">Número 2</label>
-          <input type="number" name="number2" placeholder="Segundo número" required ref={number2}/>
+          <label htmlFor="number2">Número 2</label>
+          <input
+            type="number"
+            name="number2"
+            placeholder="Segundo número"
+            required
+            ref={number2}
+          />
         </div>
         <div>
           <button>Somar</button>
         </div>
-
       </form>
       <h1>{result}</h1>
     </div>
