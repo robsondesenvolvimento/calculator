22b69e934cf4de22fe92e209d9fa72939c56cd6a
Robson Candido dos Santos Alves <robsondesenvolvimento@gmail.com>
2021-06-17 22:01:53 -0300

    Update code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const number2 = useRef(null);
   const [result, setResult] = useState(null);
 
-  const submitHandler = event => {
+  const submitHandler = (event) => {
     event.preventDefault();
     const firstValue = number1.current.value;
     const secondValue = number2.current.value;
@@ -17,31 +17,38 @@ function App() {
 
   return (
     <div className="App">
-      <form onSubmit={event => submitHandler(event)}>
+      <form onSubmit={(event) => submitHandler(event)}>
+
         <div>
           <label htmlFor="number1">Número 1</label>
           <input
             type="number"
             name="number1"
             placeholder="Primeiro número"
+            id="number1"
             required
             ref={number1}
           />
         </div>
+
         <div>
           <label htmlFor="number2">Número 2</label>
           <input
             type="number"
             name="number2"
             placeholder="Segundo número"
+            id="number2"
             required
             ref={number2}
           />
         </div>
+
         <div>
           <button>Somar</button>
         </div>
+
       </form>
+
       <h1>{result}</h1>
     </div>
   );
